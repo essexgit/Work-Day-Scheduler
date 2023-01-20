c1407819baf68e31c2f7bdb944af375e2fc3dad5
Ralph Cox <73188137+essexgit@users.noreply.github.com>
2023-01-20 22:11:56 +0000

    time and task object in separate file

---
+++ assets/js/mainObject.js
@@ -0,0 +1,47 @@
+let businessDayObj = [
+    {
+        id: "hour1",
+        time: "9AM",
+        task: ""
+    },
+    {
+        id: "hour2",
+        time: "10AM",
+        task: ""
+    },
+    {
+        id: "hour3",
+        time: "11AM",
+        task: ""
+    },
+    {
+        id: "hour4",
+        time: "12PM",
+        task: ""
+    },
+    {
+        id: "hour5",
+        time: "1PM",
+        task: ""
+    },
+    {
+        id: "hour6",
+        time: "2PM",
+        task: ""
+    },
+    {
+        id: "hour7",
+        time: "3PM",
+        task: ""
+    },
+    {
+        id: "hour8",
+        time: "4PM",
+        task: ""
+    },
+    {
+        id: "hour9",
+        time: "5PM",
+        task: ""
+    }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,33 @@ var currentDay = moment().format('dddd Do MMMM YYYY');
 $('#currentDay').text(currentDay);
 // Present timeblocks for standard business hours when the user scrolls down.
 
-
+//check local storage
 
 //  Load timeline for the day and time blocks for each hour beside the timeline;
+var nowCheck = moment();
+var time1 = moment('23', 'hh');
+
+//setting colour base on time
+var gap = time1.diff(nowCheck, 'hours');
+$('#hour1').text(gap);
+console.log(gap);
+var timeBlock1 = document.querySelector(".time-block");
+if (gap < 0) {
+    timeBlock1.classList.add('past');
+} else if (gap > 0) {
+    timeBlock1.classList.add('future');
+} else {
+    timeBlock1.classList.add('present');
+}
+
+
 
 
 // Color - code each timeblock based on past, present, and future when the timeblock is viewed.
 // Set the colour of the time blocks to past present or future, (grey, red, green)
 
 // Allow a user to enter an event when they click a timeblock
+// make box a submit form
 
 // Allow the time block to have text entered
 
