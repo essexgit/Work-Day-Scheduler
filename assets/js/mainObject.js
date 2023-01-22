a0fe6522cb3b4bb9570f98514854a75e779efca2
Ralph Cox <73188137+essexgit@users.noreply.github.com>
2023-01-22 20:26:11 +0000

    add element variables and control for main object

--- assets/js/mainObject.js
+++ assets/js/mainObject.js
@@ -1,46 +1,47 @@
+// Array of Objects creates times, block IDs and string to add entered tasks
 let businessDayObj = [
     {
-        id: "hour1",
+        id: "hour0",
         time: "9AM",
         task: ""
     },
     {
-        id: "hour2",
+        id: "hour1",
         time: "10AM",
         task: ""
     },
     {
-        id: "hour3",
+        id: "hour2",
         time: "11AM",
         task: ""
     },
     {
-        id: "hour4",
+        id: "hour3",
         time: "12PM",
         task: ""
     },
     {
-        id: "hour5",
+        id: "hour4",
         time: "1PM",
         task: ""
     },
     {
-        id: "hour6",
+        id: "hour5",
         time: "2PM",
         task: ""
     },
     {
-        id: "hour7",
+        id: "hour6",
         time: "3PM",
         task: ""
     },
     {
-        id: "hour8",
+        id: "hour7",
         time: "4PM",
         task: ""
     },
     {
-        id: "hour9",
+        id: "hour8",
         time: "5PM",
         task: ""
     }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,45 @@
 // Display the current day at the top of the calender when a user opens the planner.
 var currentDay = moment().format('dddd Do MMMM YYYY');
 $('#currentDay').text(currentDay);
-// Present timeblocks for standard business hours when the user scrolls down.
 
-//check local storage
+// save current day to localStorage 
+// set element root as container for rows
+let listRoot = $('.container');
+// set row section
+let rowSection = $('<div>');
+rowSection.addClass('row');
+// set time place
+let hour = $('<div>');
+hour.addClass('hour');
+//set textarea
+let timeBlock = $('<textarea>');
+timeBlock.addClass('timeBlock');
+//set save button
+let save = $('<button>');
+save.addClass('saveBtn');
+
+
+// put main into local if empty or if local day != current day
+if (!localStorage.busObj) {
+    localStorage.setItem('busObj', JSON.stringify(businessDayObj));
+}
+
+//  set main to local
+businessDayObj = JSON.parse(localStorage.getItem('busObj'));
+
+// Present timeblocks for standard business hours when the user scrolls down.
+//create row
+function createRow() {
+
+}
+
+// append rows for workday
+
+//  set text from main
+
+
+
+// loop through main object to populate rows
 
 //  Load timeline for the day and time blocks for each hour beside the timeline;
 var nowCheck = moment();
@@ -23,19 +59,4 @@ if (gap < 0) {
 }
 
 
-
-
-// Color - code each timeblock based on past, present, and future when the timeblock is viewed.
-// Set the colour of the time blocks to past present or future, (grey, red, green)
-
-// Allow a user to enter an event when they click a timeblock
-// make box a submit form
-
-// Allow the time block to have text entered
-
 // Save the event in local storage when the save button is clicked in that timeblock.
-// Connect a save button to save content into local storage
-
-// Collect existing content from local storage when page loads.
-
-// Persist events between refreshes of a page;
